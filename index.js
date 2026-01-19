f483d75d18dc94ba646a9fde716c0ab34910723b
Ishaant Kumar Singh <ishaant97@gmail.com>
2026-01-19 10:57:36 +0530

    add CORS middleware to enable cross-origin requests

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const connectToMongoDB = require('./connections/mongodbConfig');
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/auth.route');
+const cors = require('cors');
 require('dotenv').config();
-
 
 connectToMongoDB();
 
@@ -11,6 +11,10 @@ const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173', // Frontend URL from which requests will be accepted
+    credentials: true                // Required if you are sending cookies/sessions
+}));
 
 app.use('/api/auth', authRoutes);
 
